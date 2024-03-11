7f50c3be14a1ca608ad16b25c8b766ea2583888e
flexita24 <jurayevamehriniso1@gmail.com>
2024-03-11 16:37:28 +0500

    finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -64,17 +64,17 @@ const Navbar = () => {
               </button>
               <ul className="absolute mt-2 w-24 bg-white rounded-lg shadow-md hidden">
                 <li>
-                  <a href="#" className="block py-2 px-4 text-gray-900 hover:bg-gray-100">Female</a>
+                  <a href="#" className="block py-2 px-4 hover:bg-gray-100">Female</a>
                 </li>
                 <li>
-                  <a href="#" className="block py-2 px-4 text-gray-900 hover:bg-gray-100">Male</a>
+                  <a href="#" className="block py-2 px-4 hover:bg-gray-100">Male</a>
                 </li>
               </ul>
             </li>
             <li>
               <a
                 href="#"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                className="block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 About
               </a>
@@ -82,7 +82,7 @@ const Navbar = () => {
             <li>
               <a
                 href="#"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                className="block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 Pricing
               </a>
@@ -90,7 +90,7 @@ const Navbar = () => {
             <li>
               <a
                 href="#"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                className="block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
                 Blog
               </a>
